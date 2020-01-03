6c3968fc203fd5f241c0aa02ce83402f4abc1960
JimmyLuo0 <47548547+JimmyLuo0@users.noreply.github.com>
2020-01-02 22:54:49 -0500

    control file

---
+++ sem_control.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define KEY 24602
+#define SEG_SIZE 200
+
+union semun {
+  int              val;    /* Value for SETVAL */
+  struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+  unsigned short  *array;  /* Array for GETALL, SETALL */
+  struct seminfo  *__buf;  /* Buffer for IPC_INFO
+                              (Linux-specific) */
+};
+
+
+int main(int argc, char *argv[]) {
+  int semd;
+  int v, r;
+  char input[3];
+  int shmd;
+  char * data;
+  int fd;
+
+  if(!strncmp(argv[1], "-c", 30)){
+    semd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
+    if (semd == -1) {
+      printf("error %d: %s\n", errno, strerror(errno));
+      semd = semget(KEY, 1, 0);
+      v = semctl(semd, 0, GETVAL, 0);
+      printf("semctl returned: %d\n", v);
+    }
+    else {
+      union semun us;
+      us.val = 1;
+      r = semctl(semd, 0, SETVAL, us);
+      printf("semctl returned: %d\n", r);
+    }
+    printf("semaphore created\n");
+
+    shmd = shmget(KEY, SEG_SIZE, IPC_CREAT | 0644);
+    data = shmat(shmd, 0, 0);
+    printf("shared memory created\n");
+
+    fd = open("textfile",O_CREAT|O_TRUNC|O_EXCL,0644);
+    printf("file created\n");
+  }
+
+  if(!strncmp(argv[1], "-v", 30)){
+    char *buff;
+    read(fd, buff, 500);
+    printf("the story so far:\n %s\n", buff);
+  }
+
+  shmdt(data);
+
+  if (!strncmp(argv[1], "-r", 30)) {
+    shmctl(shmd, IPC_RMID, 0);
+    printf("segment deleted\n");
+  }
+
+  return 0;
+}
